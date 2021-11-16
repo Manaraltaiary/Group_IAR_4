f1e89c0963bd8bdcd30a0e7f68a10df6717fe6ae
Shatha04 <94395808+Shatha04@users.noreply.github.com>
2021-11-16 11:53:32 +0300

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mercyproject251;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Customer extends Account{
+    
+    public Customer(String username, String password, String name, String phoneNumber) {
+        super(username, password, name, phoneNumber);
+    }
+
+    Customer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+}
